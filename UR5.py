4829fc3e8baef4ee31dd0461ec756701170448d0
kejianhong <58462346+kejianhong@users.noreply.github.com>
2020-03-24 16:26:09 +0800

    Update UR5.py

--- UR5.py
+++ UR5.py
@@ -1,34 +1,53 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 16 18:11:01 2020
 
+@author: KJH
+"""
 from __future__ import division
 import numpy as np
 import math
 import time
 import sim
-
 ''''''
 RAD2DEG=math.pi/180
-tstep=0.1#step
+tstep=0.05
 
-#information about joint
-jointNum=6
+jointNum=6  
 baseName='UR5'
 jointName='UR5_joint'
 
-#set the motor position
-t=np.linspace(0,0.5*np.pi,200,endpoint=False)
-x=2*np.pi*np.sin(t)
+#''' state1 q1,dt=0.05'''
+#t=np.linspace(0,0.5*np.pi,400,endpoint=False)
+#x=4*np.pi*np.sin(t)
+#'''state1 q2,dt=0.05'''
+#t=np.linspace(0,4*np.pi,400,endpoint=False)
+#x=0.5*np.pi*np.sin(t)-0.5*np.pi
+#'''state1 q3,dt=0.05'''
+#t=np.linspace(0,4*np.pi,400,endpoint=False)
+#x=0.5*np.pi*np.sin(t)
+#'''state1 q4,dat=0.05'''
+#t=np.linspace(0,0.5*np.pi,400,endpoint=False)
+#x=4*np.pi*np.sin(t)-0.5*np.pi
+'''state2 q5,dt=0.05'''
+t=np.linspace(0,0.5*np.pi,400,endpoint=False)
+x=4*np.pi*np.sin(t)
+#'''q6'''
+#t=np.linspace(0,0.5*np.pi,400,endpoint=False)
+#x=4*np.pi*np.sin(t)
 
 print('Program started')
 
 ''''''
-#close all the other API
 sim.simxFinish(-1)
 
-#test per 0.2 second
 while True:
-    clientID=sim.simxStart('127.0.0.1',20000,True,True,5000,5)
-    sim.simxStartSimulation(clientID,sim.simx_opmode_oneshot)#请求开始仿真
+    clientID=sim.simxStart('127.0.0.1',19997
+                           ,True,True,5000,5)
+    sim.simxStartSimulation(clientID,sim.simx_opmode_oneshot)
+    sim.simxSynchronous(clientID,True)
+    sim.simxSynchronousTrigger(clientID)
+
     if clientID !=-1:
         break
     else:
@@ -36,53 +55,39 @@ while True:
         print('Fail connecting to remote API Server !')
 print('Connection success !')
 
-''''''
-#set the same step with the coppeliasim
-sim.simxSetFloatingParameter(clientID,sim.sim_floatparam_simulation_time_step,tstep,sim.simx_opmode_blocking)
-sim.simxSynchronous(clientID,True)#为True，client端负责触发下一个仿真程序
 
-#get the handle of the server
-_,baseHandle=sim.simxGetObjectHandle(clientID,baseName,sim.simx_opmode_blocking)#基底句柄
-#get the handle of 6 joints
+_,baseHandle=sim.simxGetObjectHandle(clientID,baseName,sim.simx_opmode_blocking)
 jointHandle=np.zeros((jointNum,),dtype=int)
 for i in range(jointNum):
     _,returnHandle=sim.simxGetObjectHandle(clientID,jointName+str(i+1),sim.simx_opmode_blocking)
     jointHandle[i]=returnHandle
-
 print('Handles available')
 
-#get the initial position of the joints
-jointConfig=np.zeros((jointNum,))
-for  i in range(jointNum):
-    _,jointPosition=sim.simxGetJointPosition(clientID,jointHandle[i],sim.simx_opmode_streaming)
-    jointConfig[i]=jointPosition
-    
+sim.simxSetFloatingParameter(clientID,sim.sim_floatparam_simulation_time_step,tstep,sim.simx_opmode_blocking)
+
+jointConfig=np.zeros((jointNum,))   
+''''''
 
 lastCmdTime=sim.simxGetLastCmdTime(clientID)
-sim.simxSynchronousTrigger(clientID)
 
-#start simulation
 while sim.simxGetConnectionId(clientID) !=-1:
+    time.sleep(2)
+    lastCmdTime=sim.simxGetLastCmdTime(clientID)
     
-    for i in range(200):
+    for i in range(400):
         currentCmdTime=sim.simxGetLastCmdTime(clientID)
         dt=currentCmdTime-lastCmdTime
+        print(i)
+        for k in range(jointNum):
+            _,jointPosition=sim.simxGetJointPosition(clientID,jointHandle[k],sim.simx_opmode_streaming)
+            jointConfig[k]=jointPosition
+        print(jointConfig[4]/RAD2DEG)
         
-        '''control code'''
-        
-        for j in range(jointNum):
-            _,jpos=sim.simxGetJointPosition(clientID,jointHandle[j],sim.simx_opmode_buffer)
-            jointConfig[j]=jpos
-            
-        #stop the simulation to set all the positioin of 6 joints at the same time
-        sim.simxPauseCommunication(clientID,True)
-        
-        '''change the number here to control different joint'''
-        for k in range(0,1):
-            sim.simxSetJointTargetPosition(clientID,jointHandle[k],x[i],sim.simx_opmode_oneshot)
-        sim.simxPauseCommunication(clientID,False)
-        lastCmdTime=currentCmdTime
+        for m in range(4,5):
+            sim.simxSetJointTargetPosition(clientID,jointHandle[m],x[i],sim.simx_opmode_oneshot)
+        sim.simxSynchronous(clientID,True)
         sim.simxSynchronousTrigger(clientID)
-        sim.simxGetPingTime(clientID)
+        lastCmdTime=currentCmdTime
+
     sim.simxFinish(-1)
-    sim.simxStopSimulation(clientID,sim.simx_opmode_oneshot)
+
